49945e2a2f359fa62be7eb7d974f471017039507
klvo <klvo.gtfo.gmail.com>
2021-01-08 10:33:31 -0300

    Add routes and StepsHeader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,14 +1,12 @@
 import './App.css';
-import Home from './Home';
-import Navbar from './Navbar';
+import Routes from './routes';
 
 function App() {
   return (
-    <>
-      <Navbar/>
-      <Home /> 
-    </>
+    <Routes />
   );
 }
 
 export default App;
+
+
